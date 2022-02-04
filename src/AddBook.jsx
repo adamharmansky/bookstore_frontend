3a5641e269d99df2c65f3438dc46b8e31b9ecaf4
Adam Harmansky <micv@firemail.cc>
2022-02-04 20:44:41 +0100

    AddBook is now functional

--- src/AddBook.jsx
+++ src/AddBook.jsx
@@ -6,6 +6,8 @@ import Button from "@material-ui/core/Button";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import './BookPage.css'
 
 const config = require("./config");
 
@@ -13,17 +15,20 @@ export default function AddBook() {
     const [title, setTitle] = React.useState("");
     const [authors, setAuthors] = React.useState("");
     const [description, setDescription] = React.useState("");
-    const [subject, setSubject] = React.useState("");
+    const [content, setContent] = React.useState("");
+    const [subject, setSubject] = React.useState({});
     const [keywords, setKeywords] = React.useState("");
-    const [readTime, setReadTime] = React.useState(0);
-    const [pageCount, setPageCount] = React.useState(0);
-    const [yearPub, setYearPub] = React.useState(0);
-    const [lang, setLang] = React.useState("");
-    const [isbn, setIsbn] = React.useState(0);
-    // const [image, setImage] = React.useState("");
+    const [hours, setHours] = React.useState('');
+    const [minutes, setMinutes] = React.useState('');
+    const [pageCount, setPageCount] = React.useState('');
+    const [yearPub, setYearPub] = React.useState('');
+    const [lang, setLang] = React.useState({});
+    const [isbn, setIsbn] = React.useState('');
+    const [image, setImage] = React.useState({});
 
     const [subjectOptions, setSubjectOptions] = React.useState([]);
     const [authorOptions, setAuthorOptions] = React.useState([]);
+    const [langOptions, setLangOptions] = React.useState([]);
 
     React.useEffect(() => {
         Axios.get(config.apiUrl + 'subject/list').then((data) => {
@@ -34,47 +39,86 @@ export default function AddBook() {
             console.log(data);
             setAuthorOptions(data.data);
         });
+        Axios.get(config.apiUrl + 'lang/short').then((data) => {
+            console.log(data);
+            setLangOptions(data.data);
+        });
     }, []);
 
     function handleSubmit(e) {
+        if (isNaN(hours) || isNaN(minutes) || isNaN(pageCount) || isNaN(yearPub) || !isbn || !title) {
+            window.alert("Prosím vložte správne hodnoty");
+            return;
+        }
+        const formData = new FormData();
+        formData.append("image", image, image.name);
+        Axios.post(config.apiUrl + "image/new", formData);
         Axios.post(config.apiUrl + "book/new/", {
             title: title,
             authors: authors,
             desc: description,
-            subject: subject,
+            content: content,
+            subject: subject.subject_id,
             keywords: keywords,
-            read_time: readTime,
+            read_time: parseInt(hours)*60 + parseInt(minutes),
             pages: pageCount,
             year_pub: yearPub,
-            lang: lang,
+            lang: lang.lang_id,
             isbn: isbn,
-            // image: image
+            image: '/pub/' + image.name
         }).then((response) => {
             console.log(response);
         });
     }
 
+    const style = {
+        "--subject-clr0": subject.subject_color0,
+        "--subject-clr1": subject.subject_color1,
+        "--subject-clr2": subject.subject_color2,
+        "--subject-bg": subject.subject_background
+    };
+
     return (
-        <div className="AddBookForm">
+        <div className="BookPageContainer" style={style}>
+            <h1> {title ? title : "Nová kniha"} </h1>
+            <h2> {authors ? authors.join(', ') : "Nezámy autor"} </h2>
+
+            <TextField fullWidth label="Názov knihy" onChange={(e) => { setTitle(e.target.value); }} />
+
+            <Autocomplete
+                multiple freeSolo fullWidth
+                label="Autori"
+                options={authorOptions.map((value) => value.author_name)}
+                renderInput={ (params)=> <TextField {...params} label='Autori' />}
+                onChange={(e,v)=>{console.log(v); setAuthors(v)}}
+            />
+
             <FormControl fullWidth>
-                <h1> {title ? title : "Nová kniha"} </h1>
-                {/* <h2> {authors ? authors : "Nezámy autor"} </h2> */}
-                <TextField label="Názov knihy" onChange={(e) => { setTitle(e.target.value); }} />
-                <Autocomplete label="Autor" options={authorOptions.map((value) => value.author_name)} renderInput={
-                    (params)=> <TextField {...params} label='Autor' />} />
-                <TextField label="Popis" multiline fullWidth onChange={(e) => { setDescription(e.target.value); }} />
-                <Select label="Predmet" onChange={(e)=>{setSubject(e.target.value)}}>
-                    {subjectOptions.map((value) => <MenuItem value={value.subject_id}>{value.subject_name}</MenuItem>)}
+                <InputLabel id='subject-select-label'> Predmet </InputLabel>
+                <Select label="Predmet" labelId='subject-select-label' id='subject-select' onChange={(e)=>{setSubject(e.target.value)}}>
+                    {subjectOptions.map((value) => <MenuItem value={value}>{value.subject_name}</MenuItem>)}
                 </Select>
-                <TextField label="Kľúčové slová" onChange={(e) => { setKeywords(e.target.value); }} />
-                <TextField label="Čas čítania" onChange={(e) => { setReadTime(e.target.value); }} />
-                <TextField label="Počet strán" onChange={(e) => { setPageCount(parseInt(e.target.value)); }} />
-                <TextField label="Rok vydania" onChange={(e) => { setYearPub(parseInt(e.target.value)); }} />
-                <TextField label="Jazyk" onChange={(e) => { setLang(e.target.value); }} />
-                <TextField label="ISBN" onChange={(e) => { setIsbn(parseInt(e.target.value)); }} />
-                <TextField label="Obrázok" onChange={(e) => { setIsbn(e.target.value); }} />
-                <Button onClick={handleSubmit}> Pridať </Button>
             </FormControl>
+
+            <TextField fullWidth label="Popis" multiline onChange={(e) => { setDescription(e.target.value); }} />
+            <TextField fullWidth label="Obsah" multiline onChange={(e) => { setContent(e.target.value); }} />
+            <TextField fullWidth label="Kľúčové slová" onChange={(e) => { setKeywords(e.target.value); }} />
+            <TextField error={isNaN(hours)} label="Hodiny" onChange={(e) => {setHours(e.target.value)}} />
+            <TextField error={isNaN(minutes)} label="Minúty" onChange={(e) => { setMinutes(e.target.value); }} />
+            <TextField fullWidth error={isNaN(pageCount)} label="Počet strán" onChange={(e) => { setPageCount(parseInt(e.target.value)); }} />
+            <TextField fullWidth error={isNaN(yearPub)} label="Rok vydania" onChange={(e) => { setYearPub(parseInt(e.target.value)); }} />
+
+            <FormControl fullWidth>
+                <InputLabel> Jazyk </InputLabel>
+                <Select label="Jazyk" onChange={(e)=>{setLang(e.target.value)}}>
+                    {langOptions.map((value) => <MenuItem value={value}>{value.lang_name}</MenuItem>)}
+                </Select>
+            </FormControl>
+
+            <TextField fullWidth label="ISBN" onChange={(e) => { setIsbn(e.target.value); }} />
+            <Button component='label'> Nahrať Obrázok <input type='file' hidden onChange={(e)=>{setImage(e.target.files[0])}}/> </Button> <span>{image.name}</span>
+
+            <Button fullWidth onClick={handleSubmit}> Pridať </Button>
         </div>
     );
 }
